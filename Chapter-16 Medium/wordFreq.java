06c739cad690762381f924828e8847e8daed0eb6
Xi Pu <xp19@duke.edu>
2019-01-02 18:43:12 -0800

    add wordFreq

---
+++ Chapter-16 Medium/wordFreq.java
@@ -0,0 +1,8 @@
+/*
+  Design a method to find the frequency of occurrences of any given word in a
+  book. What if we were running this algorithm multiple times?
+*/
+
+/*
+  Use a hashtable, key is a particular word and the value is its frequency
+*/
